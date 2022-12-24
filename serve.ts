172bb5078ac7c59499d92ae81c4c0550c1f3acea
so1ve <nn_201312@163.com>
2022-12-24 14:11:26 +0800

    ?

--- serve.ts
+++ serve.ts
@@ -2,6 +2,7 @@ import { serve } from "https://deno.land/std@0.170.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.170.0/http/file_server.ts";
 
 serve((req) => {
+  console.log(req.url);
   return serveDir(req, {
     fsRoot: "./dist",
   });
