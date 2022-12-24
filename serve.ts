b2de22041850a3c2aa364a8f44356dec1191e73b
so1ve <nn_201312@163.com>
2022-12-24 14:21:02 +0800

    static!

--- serve.ts
+++
@@ -1,6 +0,0 @@
-import { serve } from "https://deno.land/std@0.170.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.170.0/http/file_server.ts";
-
-serve((req) => {
-  return new Response("Hello World");
-});
