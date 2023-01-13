238b6bdb5227ba354e1e81da7cabf20c5b3fccc5
so1ve <nn_201312@163.com>
2023-01-13 16:42:08 +0800

    fix: pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,16 @@ export default defineNuxtConfig({
     icon: {
       fileName: "favicon.png",
     },
+    meta: {
+      name: "Ray's Homepage",
+      author: "Ray",
+      description: "Ray's Homepage",
+      theme_color: "#ffffff",
+    },
+    manifest: {
+      name: "Ray's Homepage",
+      lang: "en",
+    },
   },
   app: {
     head: {
@@ -38,12 +48,12 @@ export default defineNuxtConfig({
         { name: "theme-color", content: "#ffffff" },
         { name: "apple-mobile-web-app-capable", content: "yes" },
         { name: "apple-mobile-web-app-status-bar-style", content: "black" },
-        { name: "apple-mobile-web-app-title", content: "Ray" },
+        { name: "apple-mobile-web-app-title", content: "Ray's Homepage" },
         { name: "msapplication-TileColor", content: "#ffffff" },
         { name: "msapplication-TileImage", content: "/favicon.png" },
         { name: "application-name", content: "Ray" },
         { hid: "description", name: "description", content: "Ray's Homepage" },
-        { hid: "og:title", property: "og:title", content: "Ray" },
+        { hid: "og:title", property: "og:title", content: "Ray's Homepage" },
         { hid: "og:description", property: "og:description", content: "Ray" },
         { hid: "og:image", property: "og:image", content: "/favicon.png" },
         { hid: "og:url", property: "og:url", content: "https://mk1.io" },
@@ -51,7 +61,7 @@ export default defineNuxtConfig({
         { hid: "og:type", property: "og:type", content: "website" },
         { hid: "og:locale", property: "og:locale", content: "en_US" },
         { hid: "twitter:card", name: "twitter:card", content: "summary" },
-        { hid: "twitter:title", name: "twitter:title", content: "Ray" },
+        { hid: "twitter:title", name: "twitter:title", content: "Ray's Homepage" },
         { hid: "twitter:description", name: "twitter:description", content: "Ray's Homepage" },
         { hid: "twitter:image", name: "twitter:image", content: "/favicon.png" },
         { hid: "twitter:site", name: "twitter:site", content: "https://mk1.io" },
