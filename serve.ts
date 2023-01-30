393cd256e57f0e857eb7b6df1ee8c72b0dc4667a
so1ve <nn_201312@163.com>
2023-01-30 23:25:51 +0800

    fix: redirect

--- serve.ts
+++ serve.ts
@@ -5,10 +5,14 @@ import { serveDir } from "https://deno.land/std@0.170.0/http/file_server.ts";
 serve((req) => {
   // Redirect /.well-known/webfinger to mas.mk1.io
   if (req.url.includes("/.well-known/webfinger")) {
+    const reqURL = new URL(req.url);
+    const path = reqURL.pathname;
+    const search = reqURL.search;
+    const dest = `https://mas.mk1.io${path}${search}`;
     return new Response(null, {
       status: 301,
       headers: {
-        Location: "https://mas.mk1.io/.well-known/webfinger",
+        "Location": dest,
         "Access-Control-Allow-Origin": "*",
       },
     });
