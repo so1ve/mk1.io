98e14133e2ae1c02592591f9e7261984d2520c45
so1ve <i@mk1.io>
2026-04-21 13:25:02 +0800

    fix cors

--- server/routes/.well-known/matrix/server.get.ts
+++ server/routes/.well-known/matrix/server.get.ts
@@ -1,6 +1,10 @@
 export default defineEventHandler((event) => {
 	const host = getRequestHost(event, { xForwardedHost: false });
 
+	setResponseHeader(event, "Access-Control-Allow-Origin", "*");
+	setResponseHeader(event, "Access-Control-Allow-Methods", "GET, OPTIONS");
+	setResponseHeader(event, "Access-Control-Allow-Headers", "Content-Type");
+
 	return {
 		"m.server": `matrix.${host}:443`,
 	};
